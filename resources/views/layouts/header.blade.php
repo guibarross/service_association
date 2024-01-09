6b44edfcaaf343b6cb0f66f66403a90c3ada25a2
guibarross <guilhermecbarros7@gmail.com>
2024-01-09 15:05:26 -0300

    adicionando ação nos links do menu de navegação

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,11 @@
 <div class="card-header bg-primary shadow p-3 mb-5 border-0">
 <nav class="navbar navbar-expand-lg">
-    <a class="navbar-brand text-white" href="#">
-        <img src="/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-        Bootstrap
+    <a class="navbar-brand text-white" href="{{ route ('services.index') }}">
+         Service Association
       </a>
     <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link text-white" href="#">Serviços</a>
+        <a class="nav-link text-white" href="{{ route ('services.index') }}">Serviços</a>
         <a class="nav-link text-white" href="#">Meu Perfil</a>
         <a class="nav-link text-white" href="#">Logout <i class="bi bi-box-arrow-right"></i></a>
         <a class="nav-link text-white d-none" href="#">Login</a>
