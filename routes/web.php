ade6318c0dbdc627bcc05292d73b731a69b9b68a
guibarross <guilhermecbarros7@gmail.com>
2024-01-11 11:16:27 -0300

    adicionando middlewere-auth das rotas

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use App\Http\Controllers\Admin\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::delete('/services/{id}', [ServiceController::class, 'destroy'])->name('services.destroy');
-Route::put('/services/{id}', [ServiceController::class, 'update'])->name('services.update');
-Route::get('/services/{id}/edit', [ServiceController::class, 'edit'])->name('services.edit');
-Route::get('/services/create', [ServiceController::class, 'create'])->name('services.create');
-Route::get('/services/{id}', [ServiceController::class, 'show'])->name('services.show');
-Route::get('/', [ServiceController::class, 'index'])->name('services.index');
-Route::post('/service', [ServiceController::class, 'store'])->name('services.store');
+Route::delete('/services/{id}', [ServiceController::class, 'destroy'])->name('services.destroy')->middleware('auth');
+Route::put('/services/{id}', [ServiceController::class, 'update'])->name('services.update')->middleware('auth');
+Route::get('/services/{id}/edit', [ServiceController::class, 'edit'])->name('services.edit')->middleware('auth');
+Route::get('/services/create', [ServiceController::class, 'create'])->name('services.create')->middleware('auth');
+Route::get('/services/{id}', [ServiceController::class, 'show'])->name('services.show')->middleware('auth');
+Route::get('/', [ServiceController::class, 'index'])->name('services.index')->middleware('auth');
+Route::post('/service', [ServiceController::class, 'store'])->name('services.store')->middleware('auth');
 
 
 Route::middleware([
