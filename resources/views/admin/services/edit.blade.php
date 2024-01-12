aca7e26242f4a692489c9af3000e7ea5657168e6
guibarross <guilhermecbarros7@gmail.com>
2024-01-12 16:57:50 -0300

    ajuste no front das viwes create, show e edit

--- resources/views/admin/services/edit.blade.php
+++ resources/views/admin/services/edit.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
 
-<div class="container shadow rounded ">
-    <h4 class="pt-3 px-3">Cadastrar novo serviço</h4>
-    <hr>
-<form class="mb-3 py-3 px-3" action="{{ route ('services.update', $service->id) }}" method="post">  
+<div class="container shadow rounded px-0 my-5">
+  <div class="card-header mb-2">
+    <h4 class="pt-3 ">Cadastrar novo serviço</h4>
+  </div>
+<form class="mb-3 py-4 px-4" action="{{ route ('services.update', $service->id) }}" method="post">  
     @csrf
     @method('put')
         <div class="form-group">
@@ -21,7 +22,9 @@
           <label for="">Descrição do Serviço</label>
           <textarea class="form-control" name="description" rows="3">{{ $service->description }}</textarea>
         </div>
+        <div class="d-flex justify-content-end">
         <button type="submit" class="btn btn-primary mb-3">Salvar</button>
+      </div>
         <div>
           @if ($errors->any())
           @foreach ($errors->all() as $error)
--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="card mx-5 my-5 border-0 rounded shadow-lg">
-            <div class="card-header pb-4">
+    <div class="card mx-5 my-5 border-0 roundeed shadow-lg">
+            <div class="card-header shadow pb-4">
             <h4 class="text-secondary mr-3 mx-2">Serviços</h4>
             <div class="d-flex justify-content-between mx-2">
                 <form class="form-inline">
@@ -18,7 +18,7 @@
         <div class="card-body">
             <div class="d-flex flex-wrap justify-content-start mt-5">
                 @foreach ($services as $service)
-                    <div class="card mb-3 mx-2 shadow" style="max-width: 18rem;">
+                    <div class="card mb-3 mx-2 shadow border-0 roundeed" style="max-width: 20rem;">
                         <div class="card-header">
                             <h5 class="card-title mb-0">{{ $service->title }}</h5>
                         </div>
@@ -30,7 +30,7 @@
                             <p class="card-text"><strong>Local: </strong>{{ $service->local }}</p>
                         </div>
 
-                        <a href="{{ route('services.show', $service->id) }}" class="d-flex justify-content-end px-3 py-3"
+                        <a href="{{ route('services.show', $service->id) }}" class="d-flex justify-content-end px-3 pb-3"
                             data-bs-toggle="tooltip" title="Visualizar">
                             <i class="bi bi-eye"></i>
                         </a>
